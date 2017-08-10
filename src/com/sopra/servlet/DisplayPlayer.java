f0124fe6eac24404474ba8819234d1ca5e286c37
Manon <lmn.manon@gmail.com>
2017-08-10 15:33:30 +0200

    ajout display lpayer & tetrimino + modifs choice

---
+++ src/com/sopra/servlet/DisplayPlayer.java
@@ -0,0 +1,9 @@
+package com.sopra.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/displayplayer")
+public class DisplayPlayer extends HttpServlet {
+
+}
