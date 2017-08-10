7449132b11d62811a96e2571bc0b333b021bb21d
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-08-10 12:10:23 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/com/sopra/servlet/AddTetrimino.java
+++ src/com/sopra/servlet/AddTetrimino.java
@@ -3,7 +3,7 @@ package com.sopra.servlet;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet("/newplayer")
-public class AddPlayer extends HttpServlet {
+@WebServlet("/newtetrimino")
+public class AddTetrimino extends HttpServlet {
 
 }
