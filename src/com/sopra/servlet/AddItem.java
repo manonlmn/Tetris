0d6dd87f6f73d35be75ff93c6efeba4590415fd2
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-08-10 14:56:59 +0200

    Essai de template de communication Servlet<=>DAO pour Tetrimino

--- src/com/sopra/servlet/AddItem.java
+++ src/com/sopra/servlet/AddItem.java
@@ -9,19 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sopra.dao.TetriminoDAO;
 import com.sopra.model.Tetrimino;
 
 @WebServlet("/newitem")
 public class AddItem extends HttpServlet {
 	
+	List<Tetrimino> myTetriminoList;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// Récupération de la liste mise à jour des tétriminos
-		List<Tetrimino> myTetriminoList = (List<Tetrimino>)request.getSession().getAttribute("myTetriminoList");
+		myTetriminoList = (List<Tetrimino>)request.getSession().getAttribute("myTetriminoList");
 	}
 	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		myTetriminoList = (List<Tetrimino>)request.getSession().getAttribute("myTetriminoList");
 
 		// Création d'un Tétrimino en récupérant les attributs de formulaire
 		Tetrimino myNewTetrimino = new Tetrimino();
@@ -29,7 +32,7 @@ public class AddItem extends HttpServlet {
 		myNewTetrimino.setColourTetrimino((String)request.getSession().getAttribute("colourNewTetrimino"));
 		
 		// Envoi du tétrimino à la DAO
-		
+		TetriminoDAO.add(myNewTetrimino);
 	}
 
 }
