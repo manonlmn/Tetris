831d2031e9ce546bcbf46526607e70a155a442af
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-08-10 12:02:28 +0200

    Conception classe Tetrimino
    attributs, get/setters

--- src/com/sopra/model/Tetrimino.java
+++ src/com/sopra/model/Tetrimino.java
@@ -1,5 +1,26 @@
 package com.sopra.model;
 
 public class Tetrimino {
-
+	
+	// Attributs de tetrimino
+	String nomTetrimino;
+	String couleurTetrimino;
+	
+	
+	
+	// Get/Setters
+	public String getNomTetrimino() {
+		return nomTetrimino;
+	}
+	
+	public void setNomTetrimino(String nomTetrimino) {
+		this.nomTetrimino = nomTetrimino;
+	}
+	
+	public String getCouleurTetrimino() {
+		return couleurTetrimino;
+	}
+	public void setCouleurTetrimino(String couleurTetrimino) {
+		this.couleurTetrimino = couleurTetrimino;
+	}
 }
