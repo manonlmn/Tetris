7368dcd2d52488e0f76dedd41d1842ae33ea0853
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-08-11 10:35:54 +0200

    Prépa Modify et Delete

--- src/com/sopra/servlet/DeleteItem.java
+++ src/com/sopra/servlet/DeleteItem.java
@@ -7,11 +7,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sopra.dao.TetriminoDAO;
+import com.sopra.model.Tetrimino;
+
 @WebServlet("/DeleteItem")
 public class DeleteItem extends HttpServlet {
-       
+    
+	//******************A MODIFIER EN DELETE*******************
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		// Renvoyer vers la JSP
+		this.getServletContext().getRequestDispatcher("/WEB-INF/TetriminoAdd.jsp").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Création d'un Tétrimino en récupérant les attributs de formulaire
+				Tetrimino myNewTetrimino = new Tetrimino();
+				myNewTetrimino.setNameTetrimino(request.getParameter("nameNewTetrimino"));
+				myNewTetrimino.setColourTetrimino(request.getParameter("colourNewTetrimino"));
+				
+				// Envoi du tétrimino à la DAO
+				TetriminoDAO myTetriminoDAO = (TetriminoDAO)this.getServletContext().getAttribute("myTetriminoDAO");
+				myTetriminoDAO.add(myNewTetrimino);
+				
+				this.getServletContext().setAttribute("myTetriminoDAO", myTetriminoDAO);
+				
+				response.sendRedirect("displaytetrimino");
 	}
 
 }
--- src/com/sopra/servlet/ModifyItem.java
+++ src/com/sopra/servlet/ModifyItem.java
@@ -7,11 +7,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sopra.dao.TetriminoDAO;
+import com.sopra.model.Tetrimino;
+
 @WebServlet("/ModifyItem")
 public class ModifyItem extends HttpServlet {
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher("/WEB-INF/TetriminoModif.jsp").forward(req, resp);
+	//******************A MODIFIER EN MODIFY*******************
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Renvoyer vers la JSP
+		this.getServletContext().getRequestDispatcher("/WEB-INF/TetriminoModif.jsp").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Création d'un Tétrimino en récupérant les attributs de formulaire
+				Tetrimino myNewTetrimino = new Tetrimino();
+				myNewTetrimino.setNameTetrimino((String)request.getSession().getAttribute("nameNewTetrimino"));
+				myNewTetrimino.setColourTetrimino((String)request.getSession().getAttribute("colourNewTetrimino"));
+				
+				// Envoi du tétrimino à la DAO
+				TetriminoDAO myTetriminoDAO = (TetriminoDAO)this.getServletContext().getAttribute("myTetriminoDAO");
+				myTetriminoDAO.add(myNewTetrimino);
+				
+				this.getServletContext().setAttribute("myTetriminoDAO", myTetriminoDAO);
+				
+				response.sendRedirect("displaytetrimino");
 	}
 
 }
