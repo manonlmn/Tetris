6fe2290226094d02baff9cfe6215989723869839
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-08-22 12:06:18 +0200

    Modif tetrimino

--- src/com/sopra/model/Tetrimino.java
+++ src/com/sopra/model/Tetrimino.java
@@ -1,5 +1,7 @@
 package com.sopra.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +12,7 @@ import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="tetrimino")
-public class Tetrimino {
+public class Tetrimino implements Serializable {
 	
 	// Tetrimino's attributes
 	@Id
