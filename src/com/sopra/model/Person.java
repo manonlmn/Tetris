44a075ef0a968fa752c7862c9ab6639c6710aedf
Manon <lmn.manon@gmail.com>
2017-08-22 12:11:36 +0200

    modifs person.java

--- src/com/sopra/model/Person.java
+++ src/com/sopra/model/Person.java
@@ -2,6 +2,27 @@ package com.sopra.model;
 
 import java.io.Serializable;
 
-public class Person implements Serializable{
+import javax.persistence.*;
 
+@Entity
+@Table(name="Person")
+public class Person implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="PER_ID")
+	int idPerson;
+	
+	@Column(name="PER_USERNAME")
+	String username;
+	
+	@Column(name="PER_PWD")
+	String password;
+	
+	@Column(name="PER_LASTNAME")
+	String nom;
+	
+	@Column(name="PER_NAME")
+	String prenom;
+	
+	
 }
