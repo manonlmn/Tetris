12d8929b8cc3717c616ab98c635ed35fc1d2b133
Manon <lmn.manon@gmail.com>
2017-08-22 12:20:36 +0200

    ajout dao hibernate pour admin

---
+++ src/com/sopra/dao/hibernate/adminDaoHibernate.java
@@ -0,0 +1,40 @@
+package com.sopra.dao.hibernate;
+
+import java.util.List;
+
+import com.sopra.dao.IAdminDAO;
+import com.sopra.model.Admin;
+
+public class adminDaoHibernate implements IAdminDAO {
+
+	@Override
+	public Admin add(Admin obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Admin modify(Admin obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Admin delete(Admin obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Admin> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Admin search(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
