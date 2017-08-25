7e79e5fad971deafdf9b10e14a10968425dc8e9b
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-08-25 12:26:33 +0200

    Ajout servlet addFigure

---
+++ src/com/sopra/servlet/AddFigure.java
@@ -0,0 +1,21 @@
+package com.sopra.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AddFigure")
+public class AddFigure extends ServletInChief {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher("/WEB-INF/FigureAdd.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
