887150090c4e685600970fbcb4e10b6dd3c7addc
Manon <lmn.manon@gmail.com>
2017-08-28 13:50:22 +0200

    Modifs figure

--- src/com/sopra/servlet/ModifyFigure.java
+++ src/com/sopra/servlet/ModifyFigure.java
@@ -1,6 +1,7 @@
 package com.sopra.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.sopra.dao.IFigureDAO;
+import com.sopra.model.Block;
+import com.sopra.model.Figure;
 
 @WebServlet("/modifyFigure")
 public class ModifyFigure extends ServletInChief {
@@ -21,6 +24,13 @@ public class ModifyFigure extends ServletInChief {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int id = Integer.parseInt(req.getParameter("id"));
+		Figure figure;
+		figure = figDAO.search(id);
+		
+		List<Block> myBlocks = figure.getMyBlocks();
+		
+		
 		
 	}
 	
