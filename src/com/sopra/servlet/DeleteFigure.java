86ab828e0d1b4468908fe7de940c4f562b2b4fbb
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-08-28 14:47:55 +0200

    Ajout de DeleteFigure

--- src/com/sopra/servlet/DeleteFigure.java
+++ src/com/sopra/servlet/DeleteFigure.java
@@ -1,10 +1,31 @@
 package com.sopra.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sopra.dao.IFigureDAO;
+import com.sopra.model.Figure;
+import com.sopra.model.Tetrimino;
 
 @WebServlet("/deleteFigure")
 public class DeleteFigure extends ServletInChief {
-
 	private static final long serialVersionUID = 1L;
-
+	
+	@Autowired
+	private IFigureDAO myFigureDAO;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int idFigureToDelete = Integer.parseInt(request.getParameter("id"));
+		Figure figure = myFigureDAO.search(idFigureToDelete);
+		
+		myFigureDAO.delete(idFigureToDelete);
+		response.sendRedirect("ListFiguresTetrimino?id="+figure.getTetrimino().getIdTetrimino());
+	}
 }
