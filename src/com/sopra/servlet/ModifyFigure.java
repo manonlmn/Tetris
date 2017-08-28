47815d32c8b3710e17fdb7326bb41278311a0d0f
Manon <lmn.manon@gmail.com>
2017-08-28 12:35:14 +0200

    Creation Servlets pour modifier et supprimer les figures

---
+++ src/com/sopra/servlet/ModifyFigure.java
@@ -0,0 +1,10 @@
+package com.sopra.servlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/modifyFigure")
+public class ModifyFigure extends ServletInChief {
+
+	private static final long serialVersionUID = 1L;
+
+}
