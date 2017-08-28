886798862c999794b0ab055a47216448956e334f
Manon <lmn.manon@gmail.com>
2017-08-28 13:50:07 +0200

    Modifs ModifyFigure

--- src/com/sopra/servlet/ModifyFigure.java
+++ src/com/sopra/servlet/ModifyFigure.java
@@ -1,10 +1,30 @@
 package com.sopra.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sopra.dao.IFigureDAO;
 
 @WebServlet("/modifyFigure")
 public class ModifyFigure extends ServletInChief {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private IFigureDAO figDAO;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	
+	
 
 }
