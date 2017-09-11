9c50b90b0e978341b18c2159971849e467c22522
Manon <lmn.manon@gmail.com>
2017-09-11 16:13:28 +0200

    Modifs

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
-var app = angular.module("tpAngular", [ 'ngRoute', 'ngResource' ]);
+var app = angular.module("tpAngular", [ 'ngRoute', 'ngResource' , 'tetrisModule']);
 
 app.constant("API_URL", "http://localhost:8080/Tetris/api");
+
+//Configuration du Tetris
+app.config(function(tetrisProvider, tetriminosProvider, API_URL) {
+	tetrisProvider.setLargeur(10);
+	tetrisProvider.setHauteur(20);
+	tetrisProvider.setSizeOfTetriminos(25);
+
+	tetriminosProvider.setApiUrl(API_URL + "/tetrimino");
+});
