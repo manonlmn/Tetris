d1ef517c31f18846f47525dda8ed491457a853d2
Manon <lmn.manon@gmail.com>
2017-09-11 15:39:32 +0200

    modifs

--- app/components/play/play_controller.js
+++ app/components/play/play_controller.js
@@ -1,5 +1,26 @@
 var app = angular.module("tpAngular");
 
-app.controller("playController", function($scope, Page) {
-	Page.setTitle("Jouer");
-});
+app.controller("playController", function($scope, Page, tetrisBoard) {
+	
+		Page.setTitle("Jouer");
+
+
+		/*
+		 * Fonction de démarrage de la partie
+		 */
+		$scope.start = function() {
+			tetrisBoard.start();
+
+			angular.element(tetrisBoard).on('gameOver', this.onGameOver);
+		};
+
+
+
+		/*
+		 * Fonction de game over
+		 */
+		$scope.onGameOver = function() {
+			angular.element(tetrisBoard).off('gameOver', this.onGameOver);
+			alert('Game Over ! (débrouillez-vous ...)');
+		}
+	});
