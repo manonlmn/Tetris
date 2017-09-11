a790709506c38bcacdb0a87539750d40399a3952
Manon <lmn.manon@gmail.com>
2017-09-11 13:54:15 +0200

    Modifs listeTetrimino côté joueur !

--- app/components/listeTetriminos/tetriController.js
+++ app/components/listeTetriminos/tetriController.js
@@ -1,5 +1,8 @@
 var app = angular.module("tpAngular");
 
-app.controller("tetriController", function($scope, Page) {
+app.controller("tetriController", function($scope, Page, tetriResources) {
 	Page.setTitle("Liste des Tetriminos");
+
+  $scope.tetriminos = tetriResources.query();
+
 });
--- src/com/sopra/restcontroller/TetriminoRestController.java
+++ src/com/sopra/restcontroller/TetriminoRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sopra.dao.ITetriminoDAO;
 import com.sopra.model.Tetrimino;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value="/tetrimino")
 public class TetriminoRestController {
