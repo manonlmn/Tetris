0f4dfcd70d7d22a6eb3a1a3b02c3268cb73a7add
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-09-11 12:24:25 +0200

    Def des routes de la navbar

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,51 @@ app.config(function($routeProvider){
 	$routeProvider
 		.when("/Tetrimino_List", {
 			templateUrl : "./app/components/listeTetriminos/listeTetri.html",
-			controller : "./app/components/listeTetriminos/tetriController.js"
+			controller : "tetriController"
 		});
+});
 
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when("/Top_Scores", {
+			templateUrl : "./app/components/topscores/topscores.html",
+			controller : "scoreController"
+		});
+});
+
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when("/FAQ", {
+			templateUrl : "./app/components/faq/faq.html",
+			controller : "faqController"
+		});
+});
+
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when("/Play", {
+			templateUrl : "./app/components/play/play.html",
+			controller : "playController"
+		});
+});
+
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when("/Login", {
+			templateUrl : "./app/components/login/login.html",
+			controller : "loginController"
+		});
+});
+
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when("/Bonus", {
+			templateUrl : "./app/components/bonus/bonus.html",
+			controller : "bonusController"
+		});
 });
