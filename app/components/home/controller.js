49327a1aba1acb9e65d583df052ed5511a2899be
Manon <lmn.manon@gmail.com>
2017-09-12 15:39:49 +0200

    Modifs

--- app/components/home/controller.js
+++ app/components/home/controller.js
@@ -1,6 +1,8 @@
 
 
 
-app.controller("homeController", function($scope, Page) {
+app.controller("homeController", function($scope, Page, loginResources) {
 	Page.setTitle("Accueil");
+
+	$scope.player = loginResources.player;
 });
