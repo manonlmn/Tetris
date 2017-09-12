fe9fadb2576f022b77098199ad9020a36128e0b1
Manon <lmn.manon@gmail.com>
2017-09-12 12:11:37 +0200

    Modifs

--- app/components/play/play_controller.js
+++ app/components/play/play_controller.js
@@ -7,6 +7,13 @@ app.controller("playController", function($scope, Page, tetrisBoard, tetrisScore
 		var game = null;
 		$scope.isHidden = true;
 		$scope.isActiv = false;
+		$scope.isChoiceVS = false;
+		$scope.games = gameResources.query();
+
+	//initialisation pour les selects :
+	$(document).ready(function() {
+	    $('select').material_select();
+	 });
 		/*
 		 * Fonction de démarrage de la partie
 		 */
@@ -18,25 +25,50 @@ app.controller("playController", function($scope, Page, tetrisBoard, tetrisScore
 
 		//creation partie solo
 		$scope.newSolo = function(){
-      gameResources.add(
-				this.game : {
+			game = gameResources.add(
+				{
 	        player1 : {idPerson : 7},
 					status : false, //statut de la partie true pour finie et false pour en cours
 					type : false //boolean pour le type de partie : false = solo & true = vs
+				});
+				$scope.isHidden = false;
+				$scope.isActiv = true;
+			};
+
+			$scope.newVS = function(){
+				game = gameResources.add(
+					{
+		        player1 : {idPerson : 7},
+						status : false, //statut de la partie true pour finie et false pour en cours
+						type : true //boolean pour le type de partie : false = solo & true = vs
+					});
+					$scope.isChoiceVS = false;
+					$scope.isActiv = true;
 				};
-		)};
 
 		/*
 		 * Fonction de game over
 		 */
 		$scope.onGameOver = function() {
 			angular.element(tetrisBoard).off('gameOver', this.onGameOver);
+			game.status = true;
+			gameResources.save({id:game.idGame}, game);
 			//ajout du score dans la bdd
 			playResources.add({
 			level : tetrisScore.level,
 			lines : tetrisScore.lines,
 			points : tetrisScore.points,
-			gameResources.save({ id: this.game.idGame },), //modification de la partie -> changement du status en true + récupération de l'idée pour modifs,
+			game : game, //modification de la partie -> changement du status en true + récupération de l'idée pour modifs,
 			player : {idPerson : 7}});
+			alert("Game Over !");
+
+			game = null;
+			$scope.isHidden = true;
+			$scope.isActiv = false;
+		};
+
+		$scope.choiceVS = function(){
+			$scope.isHidden = false;
+			$scope.isChoiceVS = true;
 		}
 	});
